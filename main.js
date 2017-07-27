d27974751d16e633c50bc97c4b2cc42ce874b038
Dakota Nichols <dakotachantz@gmail.com>
2017-07-27 15:44:53 -0500

    answered q's 1 & 2

--- main.js
+++ main.js
@@ -7,40 +7,53 @@ console.log(data);
 
 
 // 1: Show me how to calculate the average price of all items.
-function question1 () {
-  // Answer:
+function question1() {
+  let avgPrice = 0;
+  let sumPrices = 0;
+  for (let i = 0; i < data.length; i++) {
+    sumPrices = sumPrices + data[i].price;
+  }
+  console.log(sumPrices);
+  avgPrice = ((sumPrices) / data.length).toFixed(2);
+  console.log("The average price is $" + avgPrice);
 }
 
 
 
 // 2: Show me how to get an array of items that cost between $14.00 and $18.00 USD
-function question2 () {
-  // Answer:
+function question2() {
+  let itemsBetween = [];
+
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].price >= 14 && data[i].price <= 18) {
+      itemsBetween.push(data[i].title);
+    }
+  } console.log(itemsBetween);
 }
 
 
 
 // 3: Which item has a "GBP" currency code? Display it's name and price.
-function question3 () {
+function question3() {
   // Answer:
 }
 
 
 // 4: Display a list of all items who are made of wood.
-function question4 () {
+function question4() {
   // Answer:
 }
 
 
 // 5: Which items are made of eight or more materials? 
 //    Display the name, number of items and the items it is made of.
-function question5 () {
+function question5() {
   // Answer:
 }
 
 
 // 6: How many items were made by their sellers?
 // Answer:
-function question6 () {
+function question6() {
   // Answer:
 }
